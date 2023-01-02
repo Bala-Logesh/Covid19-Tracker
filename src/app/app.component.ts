3f67794721e9d5340a788321b281b4ee20c5c298
bala_s <balassmp@gmail.com>
2023-01-02 12:05:46 +0530

    Created the website and finished all functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { CoronaService } from './corona.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Covid-19-Tracker';
+  countries: any;
+  country: any = '';
+  confirmed: Number = 0;
+  recovered: Number = 0;
+  deaths: Number = 0;
+
+  constructor(private corona: CoronaService) {}
+
+  isShow = false;
+  abled = true;
+
+  toggleDisplay() {
+    this.isShow = true;
+  }
+
+  toggleDisplayoff() {
+    this.isShow = false;
+    this.country = '';
+    this.confirmed = 0;
+    this.recovered = 0;
+    this.deaths = 0;
+  }
+
+  disSelect() {
+    this.abled = false;
+  }
+
+  enSelect() {
+    this.abled = true;
+  }
+
+  ngOnInit() {
+    this.corona.getCountries().subscribe((data) => {
+      this.countries = data;
+    });
+  }
+
+  getCoronaData() {
+    this.corona.getCoronaRealtimeData(this.country).subscribe((data) => {
+      var index = data.length - 1;
+      this.confirmed = data[index].Confirmed;
+      this.recovered = data[index].Recovered;
+      this.deaths = data[index].Deaths;
+    });
+  }
+
+  getCountry(country: any) {
+    this.country = this.countries.filter(
+      (cntry: any) => country === cntry.Slug
+    )[0].Country;
+  }
 }
